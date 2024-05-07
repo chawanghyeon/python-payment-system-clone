4c7f9c055853c4b36f0936631b93254c5290d4c9
chawanghyeon <chawanghyeon@gmail.com>
2024-05-08 01:42:13 +0900

    feature: user_id 가져오는 방식 변경

--- project/payments/api/v1/endpoints/orders.py
+++ project/payments/api/v1/endpoints/orders.py
@@ -4,13 +4,17 @@ from project.payments.services.order_service import OrderService
 
 class OrderView(APIView):
     def get(self, request, pk=None):
-        return OrderService.read_order(pk, user_id=request.user.id)
+        return OrderService.read_order(pk, user_id=request.data.get("user_id"))
 
     def post(self, request):
-        return OrderService.create_order(request.data, user_id=request.user.id)
+        return OrderService.create_order(
+            request.data, user_id=request.data.get("user_id")
+        )
 
     def put(self, request, pk=None):
-        return OrderService.update_order(pk, request.data, user_id=request.user.id)
+        return OrderService.update_order(
+            pk, request.data, user_id=request.data.get("user_id")
+        )
 
     def delete(self, request, pk=None):
-        return OrderService.delete_order(pk, user_id=request.user.id)
+        return OrderService.delete_order(pk, user_id=request.data.get("user_id"))
