2e3d657627ce437dc80180828ab9773710fde407
chawanghyeon <chawanghyeon@gmail.com>
2024-05-07 17:47:38 +0900

    feature: sharding_target 반환값 수정

--- project/payments/utils/sharding_target_decorator.py
+++ project/payments/utils/sharding_target_decorator.py
@@ -14,19 +14,25 @@ def sharding_target(func: Any) -> Any:
     def wrapper(*args: Any, **kwargs: Any) -> None:
         user_id = kwargs.get("user_id")
         if user_id is None:
-            return Response("user_id is required", status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "user_id is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         shard_lookup_key = ShardLookupKeyDetermineService.determine_shard(user_id)
         if shard_lookup_key is None:
             return Response(
-                "shard_lookup_key is required", status=status.HTTP_400_BAD_REQUEST
+                {"error": f"Shard lookup key for user_id {user_id} not found"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
         try:
             ShardingContextHolder.set_sharding_context(user_id, shard_lookup_key)
             result = func(*args, **kwargs)
         except Exception as e:
-            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"error": f"An error occurred: {str(e)}"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
         finally:
             ShardingContextHolder.clear_context()
 
