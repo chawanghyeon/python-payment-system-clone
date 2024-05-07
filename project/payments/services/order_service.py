8ea15ec2f6b4409fdbb45bf78cf6e8332d73df3f
chawanghyeon <chawanghyeon@gmail.com>
2024-05-07 16:40:09 +0900

    feature: @classmethod @staticmethod으로 변경, 메소드 고도화

--- project/payments/services/order_service.py
+++ project/payments/services/order_service.py
@@ -1,4 +1,5 @@
 from project.payments.models.order import Order
+from project.payments.utils.mongodb_manager import MongoDbManager
 from project.payments.utils.sharding_target_decorator import sharding_target
 from project.payments.schemas.order import (
     OrderRequestSerializer,
@@ -9,15 +10,26 @@ from rest_framework.response import Response
 
 
 class OrderService:
-    @classmethod
-    def read_order(cls, order_id: int) -> Order:
-        sharding_key = cls.get_sharding_key(order_id)
-        shard = cls.get_shard(sharding_key)
-        return shard.read_order(order_id)
+    @staticmethod
+    def read_order(order_id: int) -> Order:
+        mongo_db_manager = MongoDbManager()
+        order = mongo_db_manager.get_order_from_collection(order_id)
 
-    @classmethod
+        if not order:
+            return Response(
+                {"error": f"Order with id {order_id} does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = OrderResponseSerializer(order)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @staticmethod
     @sharding_target
-    def create_order(cls, data, user_id) -> None:
+    def create_order(user_id: int, data: dict) -> None:
         serializer = OrderRequestSerializer(data=data)
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -25,16 +37,37 @@ class OrderService:
         serializer.save()
         return Response(status=status.HTTP_201_CREATED)
 
-    @classmethod
+    @staticmethod
     @sharding_target
-    def update_order(cls, order_id: int, amount: float) -> None:
-        sharding_key = cls.get_sharding_key(order_id)
-        shard = cls.get_shard(sharding_key)
-        shard.update_order(order_id, amount)
+    def update_order(order_id: int, data: dict) -> None:
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response(
+                {"error": f"Order with id {order_id} does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
-    @classmethod
+        serializer = OrderRequestSerializer(order, data=data, partial=True)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        return Response(status=status.HTTP_200_OK)
+
+    @staticmethod
     @sharding_target
-    def delete_order(cls, order_id: int) -> None:
-        sharding_key = cls.get_sharding_key(order_id)
-        shard = cls.get_shard(sharding_key)
-        shard.delete_order(order_id)
+    def delete_order(order_id: int) -> None:
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response(
+                {"error": f"Order with id {order_id} does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        order.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
